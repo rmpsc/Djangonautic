9c2aad80ea2841dfcb4becf9259e275f0cf0600d
Romel Pascua <romelpascua14@gmail.com>
2021-06-24 15:33:12 -0700

    add models.py

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    # auto_now_add automatically adds in a date
+    date = models.DateTimeField(auto_now_add=True)
+    # add in thumbnail later
+    # add in author later
